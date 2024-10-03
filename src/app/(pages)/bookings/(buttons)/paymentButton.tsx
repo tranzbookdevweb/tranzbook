d64aa86b1296f31a56878af5e317778cb86cb5da
Coby232 <selormkn2003@gmail.com>
2024-10-03 18:47:02 +0000

    "Refactor paymentButton.tsx: import PaystackButton, remove unnecessary useEffect, and update handlePayments and button onClick"

--- src/app/(pages)/bookings/(buttons)/paymentButton.tsx
+++ src/app/(pages)/bookings/(buttons)/paymentButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { usePaystackPayment } from "react-paystack";
+import { usePaystackPayment , PaystackButton} from "react-paystack";
 
 interface PaymentButtonProps {
   busFare: number;
@@ -17,25 +17,17 @@ const PaymentButton: React.FC<PaymentButtonProps> = ({
   handleBooking,
   disabled,
 }) => {
+
   const [isClient, setIsClient] = useState(false);
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) {
-    return null;
-  }
-
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const handlePayments = usePaystackPayment({
-    channels: ["mobile_money"],
-    publicKey: "pk_live_ba22e79f57638ea1339bc93a0b40fae25e3814c8",
-    phone: "0546871870",
-    email: "sknukpezah@gmail.com",
-    currency: "GHS",
-    amount: busFare * selectedSeats.length * 100,
-  });
+  // const handlePayments = usePaystackPayment({
+  //   channels: ["mobile_money"],
+  //   publicKey: "pk_live_ba22e79f57638ea1339bc93a0b40fae25e3814c8",
+  //   phone: "0546871870",
+  //   email: "sknukpezah@gmail.com",
+  //   currency: "GHS",
+  //   amount: busFare * selectedSeats.length * 100,
+  // });
 
   const onSuccess = (reference: any) => {
     handleBooking();
@@ -46,10 +38,18 @@ const PaymentButton: React.FC<PaymentButtonProps> = ({
     alert("Payment Not Successful");
   };
 
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
+
   return (
     <button
       className={className}
-      onClick={() => handlePayments({ onSuccess, onClose })}
+      // onClick={() => handlePayments({ onSuccess, onClose })}
       disabled={disabled}>
       Book Now
     </button>
