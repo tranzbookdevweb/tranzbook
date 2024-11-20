528203b2f4e839307af575662b5848c7900aeb06
Coby232 <selormkn2003@gmail.com>
2024-11-20 05:14:33 +0000

    added bank/card transfer

--- src/app/(pages)/bookings/(buttons)/paymentButton.tsx
+++ src/app/(pages)/bookings/(buttons)/paymentButton.tsx
@@ -23,10 +23,10 @@ const PaymentButton: React.FC<PaymentButtonProps> = ({
 
 
   const handlePayments = usePaystackPayment({
-    channels: ["mobile_money"],
+    channels: ["mobile_money","card","bank","bank_transfer"],
     // in .env: NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY="pk_live_9559bad7cb4e0cdbb1570bcfd85c46d9fe8296ee"
     publicKey: process.env.NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY! as string,
-    // phone: "0546871870",
+    // phone: "0546871870", //not needed
     email: "sknukpezah@gmail.com", //use the user's email from the tranzbook users table from the db
     currency: "GHS",
     // amount: 1 * 100 , //for testing = 1ghc
