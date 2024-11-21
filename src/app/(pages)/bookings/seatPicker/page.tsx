f8675f63a93388dccd37108cee1ddd824e72c8e1
tranzbook <tranzbook1@gmail.com>
2024-11-21 19:12:56 +0000

    boundary fix

--- src/app/(pages)/bookings/seatPicker/page.tsx
+++ src/app/(pages)/bookings/seatPicker/page.tsx
@@ -1,14 +1,15 @@
 "use client";
-import React, { Suspense, useEffect, useState } from "react";
+
+import React, { useEffect, useState, Suspense } from "react";
 import BusDetails from "../(components)/busDetails";
 import SeatSelection from "../(components)/seatSelection";
 import Ticket from "../(components)/ticket";
-// import { useSearchParams } from "next/navigation";
 import { BusDetailsSkeleton } from "../(components)/Skeletons/busDetailsSkeleton";
 import { SeatSelectionSkeleton } from "../(components)/Skeletons/seatSelectionSkeleton";
 import { CircularProgress } from "@mui/material";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 
+// Interfaces
 interface Bus {
   id: string;
   plateNumber: string;
@@ -58,19 +59,16 @@ interface Trip {
   route: Route;
 }
 
-const Page: React.FC = () => {
-  // const params=useSearchParams()
-  // const tripId= params.get('tripid')
-  const [currency, setCurrency] = useState<string>("GHS"); //use currency based on the user's loc from the db
+const PageContent: React.FC = () => {
+  const searchParams = useSearchParams();
+  const tripId = searchParams.get("tripId");
+  const apiUrl = `/api/GET/getTripById?id=${tripId}`;
+
+  const [currency, setCurrency] = useState<string>("GHS");
   const [selectedSeats, setSelectedSeats] = useState<string[]>([]);
   const [isBooked, setBooked] = useState<boolean>(false);
   const [tripData, setTripData] = useState<Trip | null>(null);
-  const [busImage, setBusImage] = useState<string>(
-    "default-logo-url"
-  ); // State for bus image
-  const searchParams = useSearchParams();
-  const tripId = searchParams.get("tripId"); 
-    const apiUrl = `/api/GET/getTripById?id=${tripId}`;
+  const [busImage, setBusImage] = useState<string>("default-logo-url");
 
   useEffect(() => {
     const fetchTripData = async () => {
@@ -79,7 +77,6 @@ const Page: React.FC = () => {
         const data: Trip = await response.json();
         setTripData(data);
 
-        // Set the bus image after trip data is fetched
         if (data.bus.imageUrl) {
           const imageUrl = `https://dzviyoyyyopfsokiylmm.supabase.co/storage/v1/object/public/images/${data.bus.imageUrl}`;
           setBusImage(imageUrl);
@@ -89,24 +86,25 @@ const Page: React.FC = () => {
       }
     };
 
-    fetchTripData();
-  }, [apiUrl]);
+    if (tripId) fetchTripData();
+  }, [apiUrl, tripId]);
 
   if (!tripData) {
     return (
-      <main className='flex-1 border-t border-b bg-white dark:bg-slate-700 min-h-screen flex flex-col items-center w-full relative overflow-hidden'>
-        <div className='flex flex-row w-full max-sm:gap-5 min-h-screen max-sm:flex-col-reverse sm:max-md:flex-col-reverse '>
-          <section className='min-h-screen lg:w-72 max-sm:min-h-fit sm:max-md:min-h-fit border-r max-sm:border-t sm:max-md:border-t border-gray-200 custom-scrollbar overflow-y-auto'>
+      <main className="flex-1 border-t border-b bg-white dark:bg-slate-700 min-h-screen flex flex-col items-center w-full relative overflow-hidden">
+        <div className="flex flex-row w-full max-sm:gap-5 min-h-screen max-sm:flex-col-reverse sm:max-md:flex-col-reverse">
+          <section className="min-h-screen lg:w-72 max-sm:min-h-fit sm:max-md:min-h-fit border-r max-sm:border-t sm:max-md:border-t border-gray-200 custom-scrollbar overflow-y-auto">
             <BusDetailsSkeleton />
           </section>
           <section
-            className={`bg-white  flex flex-col items-center md:justify-start md:min-h-screen lg:px-20 md:px-0 max-sm:w-screen max-sm:p-0 md:mx-1${
+            className={`bg-white flex flex-col items-center md:justify-start md:min-h-screen lg:px-20 md:px-0 max-sm:w-screen max-sm:p-0 md:mx-1 ${
               isBooked ? "justify-center" : ""
-            } p-5 w-full rounded-lg overflow-hidden`}>
-            {isBooked === true ? (
+            } p-5 w-full rounded-lg overflow-hidden`}
+          >
+            {isBooked ? (
               <CircularProgress />
             ) : (
-              <div className=' max-sm:w-full flex flex-col items-center md:min-h-screen lg:min-h-screen  p-10 sm:max-md:p-5 max-sm:p-5 max-[320px]:p-1 md:'>
+              <div className="max-sm:w-full flex flex-col items-center md:min-h-screen lg:min-h-screen p-10 sm:max-md:p-5 max-sm:p-5 max-[320px]:p-1">
                 <SeatSelectionSkeleton />
               </div>
             )}
@@ -122,17 +120,16 @@ const Page: React.FC = () => {
     departureTime: tripDepartureTime,
     bus,
     driver,
-    branch,
     route,
   } = tripData;
 
-  const busCompany = bus?.company.name;
-  const busNumber = bus?.plateNumber;
-  const busCapacity = bus?.capacity;
+  const busCompany = bus.company.name;
+  const busNumber = bus.plateNumber;
+  const busCapacity = bus.capacity;
   const tripArrivalTime = new Date(date).toLocaleTimeString([], {
     hour: "2-digit",
     minute: "2-digit",
-  }); // Assuming you have logic to calculate arrival time based on duration
+  });
   const busExtras = "Wi-Fi, AC";
   const busDriverID = `${driver.firstName} ${driver.lastName}`;
   const busRoute = {
@@ -148,35 +145,24 @@ const Page: React.FC = () => {
     );
   };
 
-  const handleClearSeats = () => {
-    setSelectedSeats([]);
-  };
-
-  const handleSelectAllSeats = () => {
+  const handleClearSeats = () => setSelectedSeats([]);
+  const handleSelectAllSeats = () =>
     setSelectedSeats(
       Array.from(
         { length: busCapacity - 1 },
         (_, i) =>
-          `${(i + 1)
-            .toString()
-            .padStart((busCapacity - 1).toString().length, "0")}`
+          `${(i + 1).toString().padStart((busCapacity - 1).toString().length, "0")}`
       )
     );
-  };
 
-  const currentDate: Date = new Date();
-  const totalCost: number = busFare * selectedSeats.length;
+  const totalCost = busFare * selectedSeats.length;
 
   const handleBooking = async () => {
-    // use the try catch block with the trip with valid details
-    // comment out the try block for testing without making a POST
     try {
-      // please add the isBooked boolean to the prisma schema/db to eventually check the
-      // availability of the seats;
       const bookingData = {
-        userId: "userId", //please with a valid userId or it wont work
-        tripId: tripId,
-        seatNumber: 1, //should be a string[] but the schema.prisma says Int so I'll leave this to you
+        userId: "userId",
+        tripId,
+        seatNumber: 1,
       };
       const response = await fetch("/api/POST/Booking", {
         method: "POST",
@@ -184,25 +170,23 @@ const Page: React.FC = () => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(bookingData),
-        // store in the db and reflect it in /bookings/manage/tickets
       });
+
       if (response.ok) {
         alert("Booking successful");
-        // replace with <Toaster/>
       } else {
         alert("Booking failed");
       }
     } catch (error) {
       console.error(error);
     }
-    setBooked((prev) => !prev);
+    setBooked(!isBooked);
   };
 
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-    <main className='flex-1 border-t border-b bg-white dark:bg- min-h-screen flex flex-col items-center w-full relative overflow-hidden'>
-      <div className='flex flex-row w-full max-sm:gap-5 min-h-screen max-sm:flex-col-reverse sm:max-md:flex-col-reverse '>
-        <section className='min-h-screen max-sm:min-h-fit border-r max-sm:border-t sm:max-md:border-t border-gray-200 custom-scrollbar overflow-y-auto'>
+    <main className="flex-1 border-t border-b bg-white dark:bg- min-h-screen flex flex-col items-center w-full relative overflow-hidden">
+      <div className="flex flex-row w-full max-sm:gap-5 min-h-screen max-sm:flex-col-reverse sm:max-md:flex-col-reverse">
+        <section className="min-h-screen max-sm:min-h-fit border-r max-sm:border-t sm:max-md:border-t border-gray-200 custom-scrollbar overflow-y-auto">
           <BusDetails
             busImage={busImage}
             busNumber={busNumber}
@@ -222,22 +206,11 @@ const Page: React.FC = () => {
             handleBooking={handleBooking}
             isBooked={isBooked}
             id={tripData.id}
-            currentDate={currentDate}
+            currentDate={new Date()}
           />
         </section>
-        <section className='flex flex-col items-center md:justify-start md:min-h-screen lg:px-20 md:px-0 w-full rounded-lg overflow-hidden max-sm:w-screen max-sm:p-0 md:mx-1 '>
-          {isBooked === false ? (
-            <div className=' max-sm:w-full flex flex-col items-center md:min-h-screen lg:min-h-screen  p-10 sm:max-md:p-5 max-sm:p-5 max-[320px]:p-1 md:'>
-         
-              <SeatSelection
-                busCapacity={busCapacity}
-                selectedSeats={selectedSeats}
-                handleSeatSelection={handleSeatSelection}
-                handleClearSeats={handleClearSeats}
-                handleSelectAllSeats={handleSelectAllSeats}
-              />
-            </div>
-          ) : (
+        <section className="flex flex-col items-center md:justify-start md:min-h-screen lg:px-20 md:px-0 w-full rounded-lg overflow-hidden max-sm:w-screen max-sm:p-0 md:mx-1">
+          {isBooked ? (
             <Ticket
               ticketId={tripData.id}
               busNumber={busNumber}
@@ -250,28 +223,29 @@ const Page: React.FC = () => {
               currency={currency}
               tripDuration={route.duration}
               totalCost={totalCost}
-              currentDate={currentDate}
+              currentDate={new Date()}
               selectedSeats={selectedSeats}
               isBooked={isBooked}
+            />
+          ) : (
+            <SeatSelection
+              busCapacity={busCapacity}
+              selectedSeats={selectedSeats}
+              handleSeatSelection={handleSeatSelection}
+              handleClearSeats={handleClearSeats}
+              handleSelectAllSeats={handleSelectAllSeats}
             />
           )}
         </section>
       </div>
-      <style jsx>{`
-        .custom-scrollbar::-webkit-scrollbar {
-          width: 8px;
-        }
-        .custom-scrollbar::-webkit-scrollbar-thumb {
-          background-color: #b7ebf8;
-          border-radius: 10px;
-        }
-        .custom-scrollbar::-webkit-scrollbar-track {
-          background-color: #ffffff;
-        }
-      `}</style>
-    
-    </main></Suspense>
+    </main>
   );
 };
+
+const Page: React.FC = () => (
+  <Suspense fallback={<CircularProgress />}>
+    <PageContent />
+  </Suspense>
+);
 
 export default Page;
