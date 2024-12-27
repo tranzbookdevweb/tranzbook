6a9dffe22591d601c4e86807ec082cad543c06c9
Coby232 <selormkn2003@gmail.com>
2024-12-27 20:21:41 +0000

    availableSeats api fix

--- src/app/api/GET/getSeatsAvailable/route.ts
+++ src/app/api/GET/getSeatsAvailable/route.ts
@@ -4,15 +4,15 @@ import { NextRequest, NextResponse } from "next/server";
 // http://localhost:3000/bookings/seatPicker?tripId=a6623405-3742-45e0-86e4-45eb5b567e60&date=2024-12-30T00%3A00%3A00.000Z
 
 // `/api/GET/getSeatsAvailable?id=a6623405-3742-45e0-86e4-45eb5b567e60&date=2024-12-30T00%3A00%3A00.000Z`
+// no seats booked at tripId=15de2f28-cb53-4164-afcb-0c0b7e237d27
 
 export async function GET(req: NextRequest) {
   try {
     const { searchParams } = new URL(req.url);
     const tripId =
-      searchParams.get("tripId") ||
-      `a6623405-3742-45e0-86e4-45eb5b567e60`;
+      searchParams.get("tripId") ;
     const date =
-    //   searchParams.get("date") ||
+      searchParams.get("date") ||
        `2024-12-30T00%3A00%3A00.000Z`; 
 
     if (!tripId || !date) {
