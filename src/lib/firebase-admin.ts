34b268a161703d2215a37dea3114473e64b6ad72
pascalseth <pascalelikem@gmail.com>
2025-06-08 11:04:34 +0000

    project id

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -4,7 +4,7 @@ import { getAuth } from 'firebase-admin/auth';
 if (!getApps().length) {
   initializeApp({
     credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
