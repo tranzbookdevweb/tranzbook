60c8ea2b5f7c9d0234c0b3d0eaec82772f687c86
pascalseth <pascalelikem@gmail.com>
2025-06-08 10:43:53 +0000

    layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,6 +2,7 @@ import { Karla } from 'next/font/google'
 import './globals.css'
 
 import { Toaster } from '@/components/ui/toaster';
+import { Metadata } from 'next';
 
 const karla = Karla({
   weight: ["200", "300", "400", "500", "600", "700", "800"],
@@ -9,7 +10,13 @@ const karla = Karla({
   variable: "--font-karla"
 })
 
-
+export const metadata: Metadata = {
+  title: "TranzBook",
+  description: "bus login/signip",
+  icons:{
+    icon:'/pictures/logo.png',
+  }
+}
 export default function LoginLayout({
   children,
 }: {
