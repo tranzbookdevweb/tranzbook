36e4954789be9bc559c9c1478a9c6c8bc1b689fe
pascalseth <pascalelikem@gmail.com>
2025-07-23 15:17:14 +0000

    textsize

--- src/components/homeNavbuttons/Bus.tsx
+++ src/components/homeNavbuttons/Bus.tsx
@@ -14,7 +14,7 @@ const BusComponent: React.FC = () => {
   return (
     <div className='flex flex-col bg-[#def5fb] items-center overflow-x-hidden w-[100%] justify-center font-tangosans'>
       <div className='flex flex-col items-center text-center'>
-        <h1 className='w-[844px] h-[148px] font-extrabold text-[64px] leading-[70px] flex items-end text-center text-[#123C7B] flex-none order-0 flex-grow-0 max-w-[90vw] max-sm:text-[32px] max-sm:leading-[36px] max-md:text-[48px] max-md:leading-[52px] font-tangosans'>
+        <h1 className='w-[844px] h-[148px] font-extrabold text-[64px] max-lg:text-[48px] leading-[70px] flex items-end text-center text-[#123C7B] flex-none order-0 flex-grow-0 max-w-[90vw] max-sm:text-[32px] max-sm:leading-[36px] max-md:text-[48px] max-md:leading-[52px] font-tangosans'>
           Save Money, Save Time, Travel with Ease
         </h1>
         <h5 className='text-[#475467] text-[20px] my-3 max-lg:text-[1.9vh] w-full font-medium font-tangosans'>
